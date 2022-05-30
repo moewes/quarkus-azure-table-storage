d1a588aaaf9403c23c54f013822db510c9d3bedc
moewes <moewes@users.noreply.github.com>
2022-05-30 20:45:32 +0200

    Cleanup Update to Quarkus 2.9.2.FINAL

--- runtime/src/main/java/net/moewes/StorageAccountConfiguration.java
+++ runtime/src/main/java/net/moewes/StorageAccountConfiguration.java
@@ -1,6 +1,5 @@
 package net.moewes;
 
-import io.quarkus.runtime.annotations.ConfigGroup;
 import io.quarkus.runtime.annotations.ConfigItem;
 import io.quarkus.runtime.annotations.ConfigPhase;
 import io.quarkus.runtime.annotations.ConfigRoot;
